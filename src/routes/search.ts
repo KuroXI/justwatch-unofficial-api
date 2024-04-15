eb593034895a3695550b62199f62ecce2c4824b5
KuroXI <vgonzales5157@gmail.com>
2024-04-15 11:25:04 +0800

    fix: profile and backdropProfile enum

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from "fastify";
 import type { SearchQuery } from "../types/query";
 import { post } from "../request";
 import { searchQuery } from "../query/Search";
+import { BackdropProfile, PosterProfile } from "../types/variables";
 
 export default async function route(fastify: FastifyInstance) {
   fastify.get("/:query", async (request, reply) => {
@@ -14,8 +15,12 @@ export default async function route(fastify: FastifyInstance) {
       country: query.country,
       language: query.language,
       format: query.format,
-      profile: query.profile,
-      backdropProfile: query.backdropProfile,
+      profile:
+        PosterProfile[query.profile as unknown as keyof typeof PosterProfile],
+      backdropProfile:
+        BackdropProfile[
+          query.backdropProfile as unknown as keyof typeof BackdropProfile
+        ],
       filter: {
         includeTitlesWithoutUrl: query.includeTitlesWithoutUrl
           ? JSON.parse(String(query.includeTitlesWithoutUrl))
