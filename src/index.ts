82ee236f087f643da81eae3bb39ea3bc9fd35c18
KuroXI <apexjacob04@gmail.com>
2024-04-15 14:44:16 +0800

    feat: integration with severless function in vercel

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,8 @@ fastify.listen(
     console.log(`Listening to ${address}`);
   }
 );
+
+export default async function handler(req: any, res: any) {
+  await fastify.ready();
+  fastify.server.emit("request", req, res);
+}
