b3164fada5f9f5f4175d8bff55d057916259e041
KuroXI <vgonzales5157@gmail.com>
2024-04-15 10:57:54 +0800

    feat: improve README.md

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,10 @@ await fastify.register(collection, { prefix: "/collection" });
 await fastify.register(search, { prefix: "/search" });
 await fastify.register(info, { prefix: "/info" });
 
-fastify.listen({ port: Number(process.env.PORT) || 8080 }, (err, address) => {
-  if (err) throw err;
-  console.log(`Listening to ${address}`);
-});
+fastify.listen(
+  { host: "0.0.0.0", port: Number(process.env.PORT) || 8080 },
+  (err, address) => {
+    if (err) throw err;
+    console.log(`Listening to ${address}`);
+  }
+);
